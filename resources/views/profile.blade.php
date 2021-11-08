c8d9787d1e9c4b119407ac105471daeaf9443e6f
VGAGFUYAGAH <alifferdiansyah292001@gmail.com>
2021-11-08 12:09:38 +0700

    update profile page (90%)
    - Belom konek database
    - Belom ada tombol untuk update

--- resources/views/profile.blade.php
+++ resources/views/profile.blade.php
@@ -9,14 +9,14 @@
 </style>
 
 @section('content')
-    <div class = "transform w-1/2 translate-y-1/3 translate-x-1/2 border-4 border-red border-opacity-100">
-        <div class = "bg-bg shadow-md rounded-xl p-5 m-5 py-10 inline-block">
+    <div class = "grid grid-cols-5 gap-4 w-2/5 transform translate-x-3/4 translate-y-36">
+        <div class = "bg-bg shadow-md rounded-xl p-5 py-10 col-span-2">
             <img src="https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_1280.png" width = "300" height = "300" class="">
-            <input type="button" class = "shadow-md w-full py-2 px-3 leading-tight bg-bgButton cursor-pointer hover:bg-bgHover text-fgBlack transition ease-in-out" value = "Choose Photo">
+            <input type="button" class = "shadow-md w-full py-2 px-3 mt-10 leading-tight bg-bgButton cursor-pointer hover:bg-bgHover text-fgBlack transition ease-in-out" value = "Choose Photo">
         </div>
-        <div class = "w-1/2 inline-block border-4 border-blue border-opacity-100">
+        <div class = "w-full col-span-3">
             <p class = "text-fg text-xl"><b>Change Biodata</b></p>
-            <form action="/profile" method="POST" class="m-5">
+            <form action="/profile" method="POST" class="">
                 <label for="name" class="text-fg text-xl">Name</label>
                 <input type="text" id="name" name="name" class = "bg-inputBg py-2 block text-fgAlt shadow appearance-none rounded w-full leading-tight focus:outline-none focus:shadow-outline">
                 
@@ -30,8 +30,8 @@
                 <input type="text" id="description" name="description" class = "bg-inputBg py-2 block text-fgAlt shadow appearance-none rounded w-full leading-tight focus:outline-none focus:shadow-outline">
             </form>
         </div>
-        <div>
-            <input type="submit" class = "shadow rounded w-full py-2 px-3 leading-tight bg-red cursor-pointer hover:bg-bgHover text-fg transition ease-in-out inline-block" value = "LOGOUT">
+        <div class="py-1/2 col-span-5">
+            <input type="submit" class = "shadow rounded w-full py-2 px-3 mt-28 leading-tight bg-red cursor-pointer hover:bg-bgHover text-fg transition ease-in-out inline-block" value = "LOGOUT">
         </div>
     </div>
     
