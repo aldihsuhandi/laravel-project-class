6e99e747a900e5d1ad0cc5c62a8d698c55645e84
Aldih Suhandi <aldihsuhandi697@gmail.com>
2021-12-15 20:05:20 +0700

    create homepage controller and rewriting master layout and newpost page

--- resources/views/template/master.blade.php
+++ resources/views/template/master.blade.php
@@ -12,20 +12,14 @@
     <link href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap" rel="stylesheet">
 </head>
 <style>
-
     * 
     {
         font-family: 'Roboto', sans-serif;
     }
-    body
-    {
-        height: 100vh;
-        width: 100%;
-    }
 </style>
-<body>
+<body class = "w-full min-h-screen bg-bgAlt">
     @include('template.header')
-    <main class = "relative bg-bgAlt w-full h-full">
+    <main class = "relative w-full h-full flex justify-center items-center">
         @yield('content')
     </main>
 </body>
